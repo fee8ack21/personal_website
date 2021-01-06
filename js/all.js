e7a5766f7b566ec26cbd4c97c522e01f956f95d1
fee8ack21 <73264834+fee8ack21@users.noreply.github.com>
2021-01-06 22:53:30 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,226 @@
+$(document).ready(function () {
+    $(window).on("load", function (e) {
+        // remove loading effect
+        setTimeout(function () {
+            $('#loading').css({ 'opacity': 0, 'z-index': '-666' })
+            setTimeout(function () {
+                $('#loading').remove()
+            }, 1000)
+            // navbar effect
+            let topDistance = $('html ,body').scrollTop()
+            if (topDistance > 100) {
+                $('#main-nav').css({ 'background-color': 'black' })
+            } else {
+                $('#main-nav').css({ 'background-color': 'transparent' })
+            }
+            $(document).scroll(function () {
+                let topDistance = $('html ,body').scrollTop()
+                if (topDistance > 100) {
+                    $('#main-nav').css({ 'background-color': 'black' })
+                } else {
+                    $('#main-nav').css({ 'background-color': 'transparent' })
+                }
+            })
+
+            // burger effect
+            $('#burger').click(function () {
+                $(this).toggleClass('burger-open');
+                $('#navbar-nav').toggleClass('nav-open');
+            })
+
+            // lightbox effect
+            lightbox.option({
+                'resizeDuration': 200,
+                'wrapAround': true,
+                'fadeDuration': 200,
+                'maxWidth': 540,
+                'positionFromTop': 180
+            })
+
+            // banner word fade in effect
+            $('.home-content-word-lg').fadeIn(2000);
+
+            // type effect
+            var i = 0;
+            var txt = '';
+            setTimeout(typeEffect, 500);
+            function typeEffect() {
+                let contentSm = 'A Front-End Engineer / Guitarist';
+                if (i < contentSm.length) {
+                    txt += contentSm.charAt(i);
+                    $('.home-content-word-sm').html(txt)
+                    $('.home-content-word-sm').css({ 'animation': 'typeEffect 0.6s infinite' })
+                    i++;
+                    setTimeout(typeEffect, 80)
+                }
+            }
+
+            // scroll to anchor effect
+            $(".navbar-nav a[href = '#home']").click(function (e) {
+                e.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: $('#home').offset().top
+                }, 0);
+                $('#burger').removeClass('burger-open')
+                $('#navbar-nav').removeClass('nav-open');
+            });
+
+            $(".navbar-nav a[href = '#profile']").click(function (e) {
+                e.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: $('#profile').offset().top - 59
+                }, 0);
+                $('#burger').removeClass('burger-open')
+                $('#navbar-nav').removeClass('nav-open');
+            });
+
+            $(".navbar-nav a[href = '#skill']").click(function (e) {
+                e.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: $('#skill').offset().top - 59
+                }, 0);
+                $('#burger').removeClass('burger-open')
+                $('#navbar-nav').removeClass('nav-open');
+            });
+
+            $(".navbar-nav a[href = '#portfolio']").click(function (e) {
+                e.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: $('#portfolio').offset().top - 59
+                }, 0);
+                $('#burger').removeClass('burger-open')
+                $('#navbar-nav').removeClass('nav-open');
+            });
+
+            $(".home-content a[href = '#profile']").click(function (e) {
+                e.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: $('#profile').offset().top - 59
+                }, 0);
+            });
+            // pages select
+            var currentPage = 1;
+            var itemsPerPage = 5;
+            var pages = Math.ceil($('.img-frontend-wrap a img').length / itemsPerPage);
+            // 
+            eachImg();
+            // 
+            $('#pages-wrap').html('');
+            for (let i = 1; i <= pages; i++) {
+                let node = $('<a href="#" class="mx-3 p-2 page-select">' + i + '</>')
+                $('#pages-wrap').append(node);
+                function pageDisabled() {
+                    $('#pages-wrap .page-select').each(function () {
+                        if ($(this).text() == currentPage) {
+                            $(this).css({ 'pointer-events': 'none', 'color': 'gray' })
+                        } else {
+                            $(this).css({ 'pointer-events': 'auto', 'color': 'black' })
+                        }
+                    })
+                }
+                pageDisabled()
+            };
+            $('.page-select').click(function (e) {
+                e.preventDefault()
+                currentPage = $(this).text();
+                eachImg()
+                pageDisabled()
+            })
+
+            // 
+            function pageSelectChange() {
+                $('#pages-wrap').html('');
+                for (let i = 1; i <= pages; i++) {
+                    let node = $('<a href="#" class="mx-3 p-2 page-select">' + i + '</>')
+                    $('#pages-wrap').append(node);
+                    function pageDisabled() {
+                        $('#pages-wrap .page-select').each(function () {
+                            if ($(this).text() == currentPage) {
+                                $(this).css({ 'pointer-events': 'none', 'color': 'gray' })
+                            } else {
+                                $(this).css({ 'pointer-events': 'auto', 'color': 'black' })
+                            }
+                        })
+                    }
+                    pageDisabled()
+                };
+                $('.page-select').click(function (e) {
+                    e.preventDefault()
+                    currentPage = $(this).text();
+                    eachImg()
+                    pageDisabled()
+                })
+            }
+
+            // 
+
+            // 
+            function eachImg() {
+                $('.img-backend-wrap a').each(function (i, v) {
+                    if (i < itemsPerPage * currentPage && i > itemsPerPage * currentPage - (itemsPerPage + 1)) {
+                        $(this).show()
+                    } else {
+                        $(this).hide()
+                    }
+                })
+                $('.img-frontend-wrap a').each(function (i, v) {
+                    if (i < itemsPerPage * currentPage && i > itemsPerPage * currentPage - (itemsPerPage + 1)) {
+                        $(this).show()
+                    } else {
+                        $(this).hide()
+                    }
+                })
+            }
+
+            // category choose
+            $('#frontend-list').click(function (e) {
+                e.preventDefault();
+                $('.img-frontend-wrap').addClass('show')
+                $('.img-backend-wrap').removeClass('show')
+                $('#frontend-list').addClass('actived');
+                $('#backend-list').removeClass('actived');
+                currentPage = 1;
+                eachImg()
+                pageDisabled()
+                pages = Math.ceil($('.img-frontend-wrap a img').length / itemsPerPage);
+                console.log(pages)
+                pageSelectChange()
+            })
+
+            $('#backend-list').click(function (e) {
+                e.preventDefault();
+                $('.img-backend-wrap').addClass('show')
+                $('.img-frontend-wrap').removeClass('show')
+                $('#backend-list').addClass('actived');
+                $('#frontend-list').removeClass('actived');
+                currentPage = 1;
+                eachImg()
+                pageDisabled()
+                pages = Math.ceil($('.img-backend-wrap a img').length / itemsPerPage);
+                console.log(pages)
+                pageSelectChange()
+            })
+
+            // skill bar animate
+            let skillTopDistance = $('html ,body').scrollTop() + $(window).height();
+            if (skillTopDistance > $('.lightbox-wrap').offset().top) {
+                $('.skill-bar').each(function () {
+                    $(this).find('div').css({ 'color': 'white' }).animate({
+                        width: $(this).find('div').attr('data-percent')
+                    }, 2500);
+                });
+            }
+
+            $(document).scroll(function () {
+                let skillTopDistance = $('html ,body').scrollTop() + $(window).height();
+                if (skillTopDistance > $('.lightbox-wrap').offset().top) {
+                    $('.skill-bar').each(function () {
+                        $(this).find('div').css({ 'color': 'white' }).animate({
+                            width: $(this).find('div').attr('data-percent')
+                        }, 2500);
+                    });
+                }
+            })
+        }, 1000);
+    })
+})
---
+++ readme.md
@@ -0,0 +1,72 @@
+# 黃品睿（Danny Huang）
+---
+* 職位 : 前端工程師
+* 生日 : 06/21/1996
+* 居住地 : 台灣 桃園市 
+* 信箱 : fee8ack21@gmail.com
+* 電話 : 0976-904-299
+
+## 背景
+---
+* 學生時期熱愛音樂，並投入許多時間在其中，多年來不停思考音樂作為職業的發展可能，後因經濟問題決定不再堅持，於今年九月開始在資策會學習前端工程。
+
+## 學歷
+---
+* 2020 - 2021 資策會 - 前端工程師班 (1/29結訓)
+* 2019 - 2020 中國科大 - 數位音樂音效班 (肄業)
+* 2014 - 2018 中原大學 - 企業管理系 (畢業)
+
+## 經歷
+---
+* 全國高中樂團比賽 - 最佳吉他手 (Teen Power) 
+* 全國大學樂團比賽 - 冠軍/槍手 (H.O.T) 
+* 文化部樂團培訓入選 (天團接力)  
+* 文化局樂團培訓入選 (青春尬歌)  
+* 學校單位、地方政府、商業活動、音樂節等邀請演出 
+* 各大Live House合演 
+* 樂器行吉他教師
+
+## 語言
+---
+* TOEIC 880
+
+## 技能
+---
+#### HTML
+* HTML5
+* Pug
+
+#### CSS
+* CSS3
+* RWD
+* SCSS
+* Bootstrap
+
+#### JavaScript
+* ES6
+* jQuery
+* Vanilla.js
+* React.js
+
+#### Backend
+* PHP
+* Node.js
+* MySQL
+
+#### Others
+* Git
+* Gulp
+
+## 作品集
+---
+* [Personal Website （#HTML, #CSS, #jQuery, #Plugin, #RWD, #Bootstrap）](https://fee8ack21.github.io/personal_website/)
+* [Travel Information （#HTML, #CSS, #RWD, #Vanilla.js, #RWD, #AJAX）](https://fee8ack21.github.io/travel_information/)
+* [BMI （#HTML, #CSS, #RWD, #Vanilla.js）](https://fee8ack21.github.io/bmi/)
+* [Dashboard Product List （#HTML, #CSS, #PHP, #MySQL）](https://github.com/fee8ack21/dashboard_product_list)
+* [Card Flip Game （#HTML, #CSS, #RWD, #jQuery）](https://fee8ack21.github.io/card_flip_game/)
+* [Calculator （#HTML, #CSS, #RWD, #Vanilla.js）](https://fee8ack21.github.io/calculator/)
+* [To Do List （#HTML, #CSS, #RWD, #Vanilla.js）](https://fee8ack21.github.io/to_do_list/)
+* [Shopping Cart （#HTML, #CSS, #Bootstrap, #jQuery）](https://fee8ack21.github.io/shopping_cart/)
+* [Hex Restaurant （#HTML, #CSS, #RWD）](https://fee8ack21.github.io/hex_restaurant/)
+* [Hex School （#HTML, #CSS, #jQuery, #Plugin）](https://fee8ack21.github.io/hex_school/)
+* [Hair Map （#HTML, #CSS, #RWD）](https://fee8ack21.github.io/hair_map/)
